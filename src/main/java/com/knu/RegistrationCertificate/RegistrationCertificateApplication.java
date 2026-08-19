8fea4ca0270ac2324fa52b102c5d7aa0eda69a64
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-08-19 17:06:08 +0530

    application.properties Added

---
+++ src/main/java/com/knu/RegistrationCertificate/RegistrationCertificateApplication.java
@@ -0,0 +1,13 @@
+package com.knu.RegistrationCertificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationCertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationCertificateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knu/RegistrationCertificate/RegistrationCertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.RegistrationCertificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationCertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
